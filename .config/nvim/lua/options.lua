dcf8677a98f7648119da3015c4d95731d55ba651
Kalitsune <git@kalitsune.net>
2025-02-11 15:26:15 +0100

    👾 Configured nvim tabs to be 4 spaces

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,3 +2,7 @@ require "nvchad.options"
 
 -- Use this file for standard vim configuration
 vim.opt.colorcolumn = "80"
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
