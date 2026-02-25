75cedc5615f67720ca9bb94f0320822c3586099b
Kalitsune <git@kalitsune.net>
2026-02-25 11:55:26 +0800

    🌼 fix(nvim): refined the filetype-related keybinds

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,23 +1,35 @@
-require "nvchad.autocmds"
+require("nvchad.autocmds")
 
 -- add mappings
 local map = vim.keymap.set
+-- Disable mappings
+local nomap = vim.keymap.del
 
+-- Asciidoc
 vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = {"*.adoc", "*.asciidoc"},
-    callback = function()
-        map('n', '<leader>ch', '<cmd><cr><cmd>Asciidoctor2HTML<cr>', {desc = '📄 Asciidoc: Convert HTML'})
-        map('n', '<leader>cp', '<cmd>Asciidoctor2PDF<cr>', {desc = '📄 Asciidoc: Convert PDF'})
-        map('n', '<leader>cd', '<cmd>Asciidoctor2DOCX<cr>', {desc = '📄 Asciidoc: Convert DOCX'})
-        map('n', '<leader>oo', '<cmd>AsciidoctorOpenRAW<cr>', {desc = '🌐 Asciidoc: Open Raw'})
-        map('n', '<leader>op', '<cmd>AsciidoctorOpenPDF<cr>', {desc = '🌐 Asciidoc: Open PDF'})
-        map('n', '<leader>oh', '<cmd>AsciidoctorOpenHTML<cr>', {desc = '🌐 Asciidoc: Open HTML'})
-        map('n', '<leader>ox', '<cmd>AsciidoctorOpenDOCX<cr>', {desc = '🌐 Asciidoc: Open DOCX'})
-    end
+	pattern = { "*.adoc", "*.asciidoc" },
+	callback = function(event)
+		map("n", "<leader>ch", ":Asciidoctor2HTML<cr>", { buffer = event.buf, desc = "📄 Asciidoc: Convert HTML" })
+		map("n", "<leader>cp", ":Asciidoctor2PDF<cr>", { buffer = event.buf, desc = "📄 Asciidoc: Convert PDF" })
+		map("n", "<leader>cd", ":Asciidoctor2DOCX<cr>", { buffer = event.buf, desc = "📄 Asciidoc: Convert DOCX" })
+		map("n", "<leader>oo", ":AsciidoctorOpenRAW<cr>", { buffer = event.buf, desc = "🌐 Asciidoc: Open Raw" })
+		map("n", "<leader>op", ":AsciidoctorOpenPDF<cr>", { buffer = event.buf, desc = "🌐 Asciidoc: Open PDF" })
+		map("n", "<leader>oh", ":AsciidoctorOpenHTML<cr>", { buffer = event.buf, desc = "🌐 Asciidoc: Open HTML" })
+		map("n", "<leader>ox", ":AsciidoctorOpenDOCX<cr>", { buffer = event.buf, desc = "🌐 Asciidoc: Open DOCX" })
+	end,
 })
+
 vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = {"main.adoc", "main.asciidoc"},
-    callback = function()
-        vim.cmd("Asciidoctor2PDF")
-    end
+	pattern = { "main.adoc", "main.asciidoc" },
+	callback = function()
+		vim.cmd("Asciidoctor2PDF")
+	end,
+})
+
+-- Typst
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = { "*.typst" },
+	callback = function(event)
+		map("n", "<leader>oo", ":TypstPreviewToggle<cr>", { buffer = event.buf, desc = "📄 Open Typst Preview" })
+	end,
 })
--- .config/nvim/lua/plugins/typst.lua
+++ .config/nvim/lua/plugins/typst.lua
@@ -2,8 +2,5 @@ return {
 	{
 		"chomosuke/typst-preview.nvim",
 		ft = { "typst" },
-		lazy = false,
-		opts = {},
-		keys = { { "<leader>op", "<cmd>TypstPreviewToggle<cr>", desc = "📄 Open Typst Preview" } },
 	},
 }
