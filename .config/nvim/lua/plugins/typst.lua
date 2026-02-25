1c082eeaf88eace110c96bf7caf91112202e71db
Kalitsune <git@kalitsune.net>
2026-02-25 10:30:38 +0800

    🌼 feat(nvim): Added typst support

---
+++ .config/nvim/lua/plugins/typst.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"chomosuke/typst-preview.nvim",
+		ft = { "typst" },
+		lazy = false,
+		opts = {},
+		keys = { { "<leader>op", "<cmd>TypstPreviewToggle<cr>", desc = "📄 Open Typst Preview" } },
+	},
+}
